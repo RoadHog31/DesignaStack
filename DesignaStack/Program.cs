3c564022b71d2641220ad83b78afb7e763ed258d
Stephen Pino <Hotrod1979@live.com>
2020-04-12 19:56:51 +0100

    Add project files.

---
+++ DesignaStack/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DesignaStack
+{
+    partial class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            //Specifices the size of the Stack List of objects.
+            int ini = 0;
+            int max = 5;
+            for (int i = ini; i <= max; i++)
+            {
+                stack.Push(i);
+
+            }            
+
+            for (int i = ini; i <= max; i++)
+            {
+                Console.WriteLine(stack.Pop());
+
+            }
+
+            Console.ReadLine();
+
+            stack.Clear();
+
+
+
+
+
+
+        }
+    }
+}
---
+++ DesignaStack/Stack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DesignaStack
+{
+    partial class Program
+    {
+        public class Stack 
+        {
+
+            private readonly List<object> _stackList;
+                  
+            
+            public Stack()
+            {
+                _stackList = new List<object>();      
+                
+            }            
+
+            //Boxing
+            public void Push(object obj)
+            {
+                if (obj == null)
+                    throw new InvalidOperationException("You can not add a null object to the stack.");
+
+                _stackList.Add(obj);
+            }
+
+            
+            public object Pop()
+            {
+
+                if (_stackList.Count == 0)
+                    throw new InvalidOperationException("You can not add a null object to the stack.");
+
+                var index = _stackList.Count - 1;
+                var toReturn = _stackList[index];
+
+                _stackList.RemoveAt(index);
+
+                return toReturn;
+            }
+
+            public void Clear()
+            {
+                if (_stackList != null)
+                {
+                    _stackList.Clear();
+                }
+                
+            }
+        }
+    }
+}
